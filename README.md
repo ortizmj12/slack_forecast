0eedeb43d4e24779036ac677abe01bd15c840e9d
Michael Ortiz <ortizmj12@gmail.com>
2016-08-11 18:23:03 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# slack_forecast
+
+A Slack bot that sends weather reports!
+
+I use [The Dark Sky Forecast API](https://developer.forecast.io/) for the weather data.
+Also use Slack's [python-slackclient](https://github.com/slackhq/python-slackclient) for sending the messages.
+
+
+## Setup
+- Clone the repo
+..- `git clone https://github.com/ortizmj12/slack_forecast.git` or
+..- `git clone git@github.com:ortizmj12/slack_forecast.git`
+- `cd` into the repo directory and create a new virtualenv
+..- `virtualenv venv`
+- Install the dependencies
+..- `pip install -r requirements`
+- Set the *FORECAST_API* and *SLACK_TOKEN* environment variables with the appropriate values
+..- `export FORECAST_API=<your_API_here>`
+..- `export SLACK_TOKEN=<your_token_here>`
+- Set the variables in *config.yaml*
+..- Latitude/longitude of the location you want to get weather reports for
+..- Channel you want the messages sent to
+..- Name of the Slack bot user you're using
+- Activate the virtualenv
+..- `source venv/bin/activate`
+- Send the weather to Slack
+..- `python forecast.py`
+
+
+## Cron job it
+I created a cron job to automagically send the weather messages every morning. Maybe not the best way to do it, but I created a bash file to run the script. Why?
+1. Because I have environment variables set, I needed to source my .bashrc
+2. Because I did this in a virtualenv, I needed to use that virtual environment's python executable
+
+Here's the syntax of the bash script:
+```
+#!/bin/bash
+
+source /home/user/.bashrc
+cd /home/user/git/slack_forecast/
+/home/user/git/slack_forecast/venv/bin/python /home/user/git/slack_forecast/forecast.py
+```
--- forecast.py
+++ forecast.py
@@ -1,20 +1,34 @@
+#!/usr/bin/env python
+
 import os
+import yaml
 import requests
 from slackclient import SlackClient
 
-url = "https://api.forecast.io/forecast/"
-forecastAPI = os.environ['FORECAST_API']
-pdx = "45.5231,-122.6765"
+forecast_url = "https://api.forecast.io/forecast/"
+forecast_api = os.environ['FORECAST_API']
 slackToken = os.environ['SLACK_TOKEN']
 sc = SlackClient(slackToken)
 
-response = requests.get(url + forecastAPI + "/" + pdx)
-json_data = response.json()
+def yaml_loader():
+    with open("config.yaml", "r") as file_descriptor:
+        data = yaml.load(file_descriptor)
+        location = data.get('variables')['location']
+        channel = data.get('variables')['channel']
+        username = data.get('variables')['username']
+    return location, channel, username
 
-weekSummary = json_data['daily']['summary']
-todaySummary = json_data['daily']['data'][0]['summary']
-todayHigh = json_data['daily']['data'][0]['temperatureMax']
-todayLow = json_data['daily']['data'][0]['temperatureMin']
-weather = "```%s```\n*Today's Weather*\n  Summary: %s\n  High: %s\n  Low: %s" % (weekSummary, todaySummary, todayHigh, todayLow)
+def get_weather(location):
+    response = requests.get(forecast_url + forecast_api + "/" + location)
+    json_data = response.json()
+    weekSummary = json_data['daily']['summary']
+    todaySummary = json_data['daily']['data'][0]['summary']
+    todayHigh = json_data['daily']['data'][0]['temperatureMax']
+    todayLow = json_data['daily']['data'][0]['temperatureMin']
+    weather = "```%s```\n*Today's Weather*\n  Summary: %s\n  High: %s\n  Low: %s" % (weekSummary, todaySummary, todayHigh, todayLow)
+    return weather
 
-sc.api_call("chat.postMessage", channel="pdx_weather", text=weather, username="slacker")
+if __name__ == "__main__":
+    location, channel, username = yaml_loader()
+    message = get_weather(location)
+    sc.api_call("chat.postMessage", channel=channel, text=message, username=username)
